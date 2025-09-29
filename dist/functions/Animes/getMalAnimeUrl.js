a2a716913a1d0ed2e33b78519502b4725e15d433
Yatsuu <pieters.lucas87@gmail.com>
2025-09-29 03:32:17 +0200

    Deleting of specific getMalUrl files

--- dist/functions/Animes/getMalAnimeUrl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMalAnimeUrl = getMalAnimeUrl;
-async function getMalAnimeUrl(nameJa) {
-    return await fetchMalUrlIfMatches('anime', nameJa);
-}
--- dist/functions/Mangas/getMalMangaUrl.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMalMangaUrl = getMalMangaUrl;
-const fetchMalUrlIfMatches_1 = require("../mal/fetchMalUrlIfMatches");
-async function getMalMangaUrl(nameJa) {
-    return await (0, fetchMalUrlIfMatches_1.fetchMalUrlIfMatches)('manga', nameJa);
-}
