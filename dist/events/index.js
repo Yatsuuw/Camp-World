2e478766fbbf26901f1eab1cdd98c810a0522adf
Yatsuu <pieters.lucas87@gmail.com>
2026-03-23 01:22:39 +0100

    Recompilation du projet

--- dist/events/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.events = void 0;
-const clientReady_1 = require("./client/clientReady");
-const interactionCreate_1 = require("./commands/interactionCreate");
-exports.events = [
-    clientReady_1.readyEvent,
-    interactionCreate_1.interactionCreate,
-];
